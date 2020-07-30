ec90c3500b871f56667b5b5b27c183c8fad8adf1
Daniel Isidro <57751686+daniel-isidro@users.noreply.github.com>
2020-07-30 18:14:44 +0200

    Update hotnpop.py
    "now the app starts with an empty search box"

--- hotnpop.py
+++ hotnpop.py
@@ -9,8 +9,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 st.image('hnp_logo.jpeg', use_column_width=True, width=None, format='JPEG')
 
-model = pickle.load(open("model.pkl","rb"))
-
 with open('settings.env') as f:
     env_vars = json.loads(f.read())
 os.environ['SPOTIPY_CLIENT_ID'] = env_vars['SPOTIPY_CLIENT_ID']
@@ -18,25 +16,25 @@ os.environ['SPOTIPY_CLIENT_SECRET'] = env_vars['SPOTIPY_CLIENT_SECRET']
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-query = st.text_input('Enter song name or artist + song name', 'Rick Astley Never Gonna Give You Up')
+model = pickle.load(open("model.pkl","rb"))
 
-if query == '':
-  query = 'Rick Astley Never Gonna Give You Up'
+query = st.text_input('Enter song name or artist + song name')
 
-audio_features = sp.audio_features((sp.search(q = query, type='track', market='US'))['tracks']['items'][0]['id'])
-X = pd.json_normalize(audio_features[0])
-X_clean = X.drop(['type', 'id', 'uri', 'track_href', 'analysis_url'], axis=1)
-
-if (model.predict(X_clean)[0])==0:
-    st.write('If released today, it would be **NOT HOT** :cry: Probability', int((model.predict_proba(X_clean)[0][0])*100), '%')
-else:
-    st.balloons()
-    st.write('If released today, it would be **HOT!!!** :smile: Probability', int((model.predict_proba(X_clean)[0][1])*100), '%')
-
-track = sp.track( X['id'][0] )
-st.write('Artist: ', pd.json_normalize(track)['artists'][0][0]['name'])
-st.write('Song:   ', pd.json_normalize(track)['name'][0])
-st.write('Album:  ', pd.json_normalize(track)['album.name'][0])
-st.write('Year:   ', pd.json_normalize(track)['album.release_date'][0][0:4])
-st.audio(pd.json_normalize(track)['preview_url'][0])
-st.image(pd.json_normalize(track)['album.images'][0][0]['url'], use_column_width=True, width=None)
+if query != '':
+  audio_features = sp.audio_features((sp.search(q = query, type='track', market='US'))['tracks']['items'][0]['id'])
+  X = pd.json_normalize(audio_features[0])
+  X_clean = X.drop(['type', 'id', 'uri', 'track_href', 'analysis_url'], axis=1)
+  if (model.predict(X_clean)[0])==0:
+      st.write('If released today, it would **NOT** be **HOT** :cry: Probability', int((model.predict_proba(X_clean)[0][0])*100), '%')
+  else:
+      st.balloons()
+      st.write('If released today, it would be **HOT!!!** :smile: Probability', int((model.predict_proba(X_clean)[0][1])*100), '%')
+  track = sp.track(X['id'][0])
+  st.write('Artist: ', pd.json_normalize(track)['artists'][0][0]['name'])
+  st.write('Song:   ', pd.json_normalize(track)['name'][0])
+  st.write('Album:  ', pd.json_normalize(track)['album.name'][0])
+  st.write('Year:   ', pd.json_normalize(track)['album.release_date'][0][0:4])
+  preview = pd.json_normalize(track)['preview_url'][0]
+  if preview is not None:
+    st.audio(preview)
+  st.image(pd.json_normalize(track)['album.images'][0][0]['url'], use_column_width=True, width=None)
